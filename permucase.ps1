30d146ac0a8243095acf97107857226482324c76
mtroisi <40772675+mtroisi@users.noreply.github.com>
2020-11-20 14:19:58 -0500

    Add files via upload
    No longer outputs duplicate results when non-alpha characters are present in the input string.

--- permucase.ps1
+++ permucase.ps1
@@ -3,30 +3,55 @@
 )
 
 $String = $String.ToLower()
-
 $Length = $String.Length
 
-$MaxPermutations = [Math]::Pow(2, $Length) - 1
+[String]$AlphaMask = ''
+$NumNotAlpha = 0
+
+# create mask to check for non alpa characters
+0..($Length - 1) | foreach {
+    if ($String[$_] -match '[a-z]') {
+        $AlphaMask += '1'    
+    }
+    else {
+        $AlphaMask += '0'
+        $NumNotAlpha++
+    }
+}
+
+
+$MaxPermutations = ([Math]::Pow(2, $Length) - 1)
 
 # outer loop goes through each permutation
-for ($i = 0; $i -le $MaxPermutations; $i++) {
+for ($index = $i = 0; $i -le $MaxPermutations; $i++) {
 
     # calculate bitmask for case
-    $mask = ([String][convert]::ToString($i,2))
+    $mask = ([String][convert]::ToString($index,2))
     $mask = $mask.PadLeft($Length, '0')
+    
+    # if mask is invalid try another
+    $check = [convert]::ToString(([convert]::ToInt32($mask,2) -band [convert]::ToInt32($AlphaMask,2)),2)
+    if ($mask -ne $check.PadLeft($Length, '0')) {
+        $index++
+        continue
+    }
 
-    $index = 0
-    $tmp = ''
+    $index++
+
+    [String]$tmp = ''
     
     # inner loop goes through each character
     0..$Length | foreach {
-        if ($mask[$index] -eq '0') {
-            $tmp += $String[$index]
+        # check if character is valid alpha
+        if ($AlphaMask[$_] -eq '0') {
+            $tmp += $String[$_]
         }
-        elseif ($mask[$index] -eq '1') {
-            $tmp += $String[$index].ToString().ToUpper()
+        elseif ($mask[$_] -eq '0') {
+            $tmp += $String[$_]
         }
-        $index++        
+        elseif ($mask[$_] -eq '1') {
+            $tmp += $String[$_].ToString().ToUpper()
+        }       
     }
 
     Write-Output $tmp
